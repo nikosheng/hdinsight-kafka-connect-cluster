3a9fc852792269f565e1322bac5d194233da01f0
Cloud User <sshuser@dev-vscode.qwijspupg0bejckdvnrdoqgy5g.bx.internal.cloudapp.net>
2022-03-07 07:17:27 +0000

    update init script

--- scripts/EmptyNodeSetup.sh
+++ scripts/EmptyNodeSetup.sh
@@ -15,7 +15,9 @@ export clusterName=$(curl -u admin:$password -sS -G "http://headnodehost:8080/ap
 export KAFKAZKHOSTS=$(curl -sS -u admin:$password -G https://$clusterName.azurehdinsight.net/api/v1/clusters/$clusterName/services/ZOOKEEPER/components/ZOOKEEPER_SERVER | jq -r '["\(.host_components[].HostRoles.host_name):2181"] | join(",")' | cut -d',' -f1,2);
 export KAFKABROKERS=$(curl -sS -u admin:$password -G https://$clusterName.azurehdinsight.net/api/v1/clusters/$clusterName/services/KAFKA/components/KAFKA_BROKER | jq -r '["\(.host_components[].HostRoles.host_name):9092"] | join(",")' | cut -d',' -f1,2);
 
-sudo bash -c 'echo $clusterName > /kafka/connect/scripts/test'
+echo $clusterName
+echo $KAFKAZKHOSTS
+echo $KAFKABROKERS
 
 ## install confluent hub client
 cd /kafka/connect/tools/
